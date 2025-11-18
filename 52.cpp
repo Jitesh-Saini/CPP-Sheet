e001b8c564b79e740b4df04a70f73f3c148c0cfa
jitesh <iamjiteshsaini2004@gmail.com>
2025-11-19 01:12:27 +0530

    updated

--- 52.cpp
+++ 52.cpp
@@ -1,26 +1,55 @@
-// 52. Take a sentence as input and print each word on a new line.  
+// // 52. Take a sentence as input and print each word on a new line.  
 
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main() {
+//     string sentence;
+//     cout << "Enter a sentence: ";
+//     getline(cin, sentence);
+
+//     for (int i = 0; i < sentence.length(); i++) {
+//         if (sentence[i] == ' ') {
+//             cout << endl;
+//         } else {
+//             cout << sentence[i];
+//         }
+//     }
+
+//     // for (char x : sentence) {
+//     //     if (x == ' ') {
+//     //         cout << endl;
+//     //     } else {
+//     //         cout << x;
+//     //     }
+//     // }
+//     return 0;
+// }
+
+
+//print in reverse order
+#include <iostream>
+#include <string>
 using namespace std;
+
 int main() {
     string sentence;
     cout << "Enter a sentence: ";
-    cin>>sentence;
+    getline(cin, sentence);
 
-    for (int i = 0; i < sentence.length(); i++) {
+    string word = "";
+
+    // Pehle last word find karne ke liye pura loop chalao
+    for (int i = sentence.length() - 1; i >= 0; i--) {
         if (sentence[i] == ' ') {
-            cout << endl;
+            cout << word << " ";
+            word = "";
         } else {
-            cout << sentence[i];
+            word = sentence[i] + word;
         }
     }
 
-    // for (char x : sentence) {
-    //     if (x == ' ') {
-    //         cout << endl;
-    //     } else {
-    //         cout << x;
-    //     }
-    // }
+    // Last word print
+    cout << word;
+
     return 0;
 }
