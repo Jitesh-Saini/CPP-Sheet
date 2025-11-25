ffeadea3c403f3839b6c37108b00d4d302cadd4f
jitesh <iamjiteshsaini2004@gmail.com>
2025-11-26 00:09:10 +0530

    file added

--- 61.cpp
+++ 61.cpp
@@ -1,4 +1,27 @@
 // 61. Take an integer input and print whether it is a prime number.  
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    int count = 0;
+
+    for (int i = 1; i <= num; i++) {
+        if (num % i == 0) {
+            count++;
+        }
+    }
+
+    if (count == 2)
+        cout << "Prime number";
+    else
+        cout << "Not a prime number";
+
+    return 0;
+}
 
 
